20d2c5fe48927fdb2f41eed34c4530b6adf8b33d
arthurdavidonisrd <davidonis007gold@gmail.com>
2025-05-16 14:37:33 -0300

    Detecting faces with rectangle ( still with false positive )

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ image = cv2.imread("assets/faces.png")
 image = cv2.resize(image,(800, 600))
 
 imagem_cinza = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Adjustments about the image ⤴️
 
-print(imagem_cinza.shape)
 
-cv2.imshow("My image", imagem_cinza)
+facial_detector = cv2.CascadeClassifier("assets/haarcascade_frontalface_default.xml")
+detections = facial_detector.detectMultiScale(imagem_cinza)
+
+for x,y,w,h in detections:
+  cv2.rectangle(image, (x,y), (x + w, y + h), (0, 255, 255), 5)
+cv2.imshow("Points in faces", image)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
