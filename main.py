9436ca909824ed8894335f6911ab36bfec8cf3ff
arthurdavidonisrd <davidonis007gold@gmail.com>
2025-05-16 14:01:53 -0300

    Resizing image and turning black and white

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import cv2
 
+image = cv2.imread("assets/faces.png")
+
+image = cv2.resize(image,(800, 600))
+
+imagem_cinza = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+print(imagem_cinza.shape)
+
+cv2.imshow("My image", imagem_cinza)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
